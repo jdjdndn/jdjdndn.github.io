f10c2ccd432b6c6b00155b691ea6544284f005e6
yu cheng <2667389861@qq.com>
2026-09-20 17:01:36 +0800

    不用open.html中转。修复大屏导航栏跟随页面滚动

--- src/about.js
+++ src/about.js
@@ -1,14 +1,36 @@
 import { friendLinks } from './data.js';
 
+// 链接图标映射
+const LINK_ICONS = {
+  '电影票': '🎬',
+  '花店': '💐',
+  '寄快递': '📦',
+  '上门回收': '♻️',
+  'fq1': '🏷️',
+  'fq2': '🏷️',
+  '苏宁易购': '🛒',
+  '当当网': '📚',
+  '1688': '🏭',
+};
+
 const el = document.getElementById('about-friend-links');
 if (el) {
-  const isWeChat = /MicroMessenger/i.test(navigator.userAgent);
   el.innerHTML = friendLinks.map((f) => {
-    let url = f.url;
-    if (isWeChat && /^https?:\/\//.test(url)) {
-      url = './open.html?url=' + encodeURIComponent(url);
-    }
-    const desc = f.description ? ` title="${f.description.replace(/"/g, '&quot;')}"` : '';
-    return `<a href="${url}" target="_blank" rel="noopener sponsored" class="friend-link"${desc}>${f.name}</a>`;
+    const href = f.url || f.link || '#';
+    const icon = LINK_ICONS[f.name] || '🔗';
+    const desc = f.description
+      ? `<span class="friend-link-desc">${f.description}</span>`
+      : '';
+    const titleAttr = f.description
+      ? ` title="${f.description.replace(/"/g, '&quot;')}"`
+      : '';
+    return `<a href="${href}" target="_blank" rel="noopener sponsored" class="friend-link"${titleAttr}>
+      <span class="friend-link-name">
+        <span class="link-icon">${icon}</span>
+        ${f.name}
+        <span class="link-arrow">›</span>
+      </span>
+      ${desc}
+    </a>`;
   }).join('');
 }
